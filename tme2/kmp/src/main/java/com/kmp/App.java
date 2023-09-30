022dde4a0831687c9cf428eb156f141e895da9d0
Aboubacar Diawara <aboubacardiawara909@gmail.com>
2023-09-30 21:45:02 +0200

    update

--- tme2/kmp/src/main/java/com/kmp/App.java
+++ tme2/kmp/src/main/java/com/kmp/App.java
@@ -1,9 +1,28 @@
 package com.kmp;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.kmp.searchengine.impl.KMPWithLPS;
+import com.kmp.searchengine.interfaces.ISearchEngine;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ISearchEngine kmpImplementation = new KMPWithLPS();
+        try (FileReader fileReader = new FileReader("babylone.txt")) {
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                kmpImplementation.search(line, "Saragon");
+            }
+
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
