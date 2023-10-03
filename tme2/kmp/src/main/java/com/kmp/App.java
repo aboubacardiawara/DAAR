b812646b681216c65c7e8cbaaf41ab851060382e
aboubacardiawara <aboubacardiawara909@gmail.com>
2023-10-03 11:15:08 +0200

    implementation des bases de l'xp

--- tme2/kmp/src/main/java/com/kmp/App.java
+++ tme2/kmp/src/main/java/com/kmp/App.java
@@ -1,14 +1,5 @@
 package com.kmp;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.sql.Time;
-
-import com.kmp.searchengine.interfaces.ISearchEngine;
-import com.kmp.searchengine.impl.KMPWithLPS;
-import com.kmp.searchengine.impl.KMPWithOptimizedLPS;
-
 public class App {
 
     public static void main(String[] args) {
@@ -16,33 +7,10 @@ public class App {
             System.out.println("Usage: mvn exec:java -Dexec.args=\"<pattern> <text_file>\"");
             System.exit(1);
         }
-        String pattern = args[0];
-        String fileName = args[1];
-        System.out.println("Pattern : " + pattern);
-        System.out.println("File : " + fileName);
         Long startTime = System.currentTimeMillis();
-        ISearchEngine kmpImplementation = new KMPWithOptimizedLPS();
-        try (FileReader fileReader = new FileReader(fileName)) {
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line;
-            int lineCount = 0;
-            while ((line = bufferedReader.readLine()) != null) {
-                int position = kmpImplementation.search(line, pattern);
-                if (position != -1) {
-                    System.out.println(position + " : " + line);
-                    lineCount++;
-                }
-            }
-
-            fileReader.close();
-            System.out.println(lineCount + " lignes contiennent le mot Sargon");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        new Finder().finder(args[1], args[0]);
         Long endTime = System.currentTimeMillis();
         System.out.println("Temps d'execution : " + (endTime - startTime) + " ms");
-        
-
     }
 
 }
--- tme2/kmp/src/main/java/com/kmp/Experimentation.java
+++ tme2/kmp/src/main/java/com/kmp/Experimentation.java
@@ -1,7 +1,99 @@
 package com.kmp;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Experimentation {
     public static void main(String[] args) {
-        System.out.println("Experimentation");
+        String fileName = "xp_result.csv";
+        try (FileWriter fileWriter = new FileWriter(fileName)) {
+            writeDuration(fileWriter);
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
+
+    private static void writeDuration(FileWriter fileWriter) throws IOException {
+        int linesCount = 100;
+        XpResult[] results = new XpResult[linesCount];
+        String pattern = "Sargon";
+        String fileName = "note.txt";
+        Finder finder = new Finder();
+        for (int i = 0; i < linesCount; i++) {
+            // measure naive implementation time
+            Long startTime = System.currentTimeMillis();
+            finder.finder(pattern, fileName);
+            Long endTime = System.currentTimeMillis();
+            Long naiveDuration = endTime-startTime;
+
+            // measure lps implementation time
+            startTime = System.currentTimeMillis();
+            finder.setSearchEngineToKmpWithLPS();
+            finder.finder(pattern, fileName);
+            endTime = System.currentTimeMillis();
+            Long lpsDuration = endTime-startTime;
+
+            // measure optimized lps implementation time
+            startTime = System.currentTimeMillis();
+            finder.setSearchEngineToKmpWithOptimizedLPS();
+            finder.finder(pattern, fileName);
+            endTime = System.currentTimeMillis();
+            Long optimizedLpsDuration = endTime-startTime;
+
+            XpResult xpResult = new XpResult();
+            xpResult.setLigneCount(i);
+            xpResult.setNaiveDuration(naiveDuration);
+            xpResult.setLpsDuration(lpsDuration);
+            xpResult.setOptimizedLpsDuration(optimizedLpsDuration);
+
+            results[i] = xpResult;
+        }
+
+        // ecriture de l'entete du fichier
+        String enTete = "#nb_line naive_duration lps_duration optimized_lps_duration";
+        fileWriter.write(enTete);
+        // ecriture du corps du fichier
+        for (int i = 0; i < linesCount; i++) {
+            XpResult result = results[i];
+            String line = result.getLigneCount() + " " + result.getNaiveDuration() + " " + result.getLpsDuration() + " " + result.getOptimizedLpsDuration() + "\n";
+            fileWriter.write(line);   
+        }
+
+    }
+}
+
+
+class XpResult {
+    private int ligneCount;
+    private Long naiveDuration;
+    private Long lpsDuration;
+    private Long optimizedLpsDuration;
+
+    public int getLigneCount() {
+        return ligneCount;
+    }
+    public void setLigneCount(int ligneCount) {
+        this.ligneCount = ligneCount;
+    }
+    public Long getNaiveDuration() {
+        return naiveDuration;
+    }
+    public void setNaiveDuration(Long naiveDuration) {
+        this.naiveDuration = naiveDuration;
+    }
+    public Long getLpsDuration() {
+        return lpsDuration;
+    }
+    public void setLpsDuration(Long lpsDuration) {
+        this.lpsDuration = lpsDuration;
+    }
+    public Long getOptimizedLpsDuration() {
+        return optimizedLpsDuration;
+    }
+    public void setOptimizedLpsDuration(Long optimizedLpsDuration) {
+        this.optimizedLpsDuration = optimizedLpsDuration;
+    }
+    
 }
